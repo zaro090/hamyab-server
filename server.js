81b0316784a7306763a21614fe1b86849795ad61
zaro090 <zerzor13or@gmail.com>
2026-08-28 01:35:23 +0330

    اصلاح پورت برای Render

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const HOST = "0.0.0.0";
 
 const users = new Map();
